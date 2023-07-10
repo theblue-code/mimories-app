8ef79f63993789eee5f296880104050151c9ab61
Yazan Unis <yazanunis89@gmail.com>
2023-07-10 15:18:06 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+"use client"
+
+import { useState, useEffect } from "react"
+import axios from "axios"
+import moment from 'moment'
+import memories from '../public/images/memories.png'
+import Image from "next/image"
+import Spinner from "@/components/Spinner"
 
 export default function Home() {
+  const [data, setData] = useState()
+  const [posts, setPosts] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    let formData = {
+      creator: e.target.creator.value,
+      title: e.target.title.value,
+      message: e.target.message.value,
+      date: e.target.date.value,
+      createdAt: new Date()
+    }
+    setData(formData)
+    e.target.reset()
+  }
+
+  const handleDelete = async (item) => {
+    const id = item._id
+    if(confirm("Are You sure want to delete the memory ?")) {
+      try {
+        await axios.delete(`https://memories-api-pi.vercel.app/posts/${id}`)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if(data) {
+      axios.post("https://memories-api-pi.vercel.app/posts",data)
+      setData(null)
+    }
+  }, [data])
+
+  useEffect(() => {
+    fetch("https://memories-api-pi.vercel.app/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+  }, [posts])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="flex bg-cyan-800 text-white shadow items-center justify-between rounded py-10 px-20">
+        <h1 className="text-sky-500 text-4xl font-extrabold">Memories</h1>
+        <Image className="inline" src={memories} width={50} height={50} />
+      </div>
+      <div className="flex flex-col sm:grid grid-cols-2 gap-4">
+        <div className="w-auto px-20 py-10">
+          <h2 className="pb-5 text-sky-500 font-bold">Create a memory!</h2>
+          <form action="http://localhost:5000/posts" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="creator"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Creator
+              </label>
+              <input
+                type="text"
+                name="creator"
+                id="creator"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Your Name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Your Memory Name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Message
+              </label>
+              <textarea
+                type="text"
+                name="message"
+                id="message"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Your Message"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="date"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Date
+              </label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Date Of Your Memory"
+                required
+              />
+            </div>
+            <div className="pt-5">
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          
+        </div>
+        <div className="flex flex-col sm:grid grid-cols-2 gap-4 py-10 px-20">
+          {!posts.length ? <Spinner />
+            : posts.map((i) => {
+              return(
+                <div key={i._id} className="relative bg-white border border-gray-200 w-auto h-auto rounded-lg shadow">
+                    <div className="p-5">
+                      <h5 className="text-2xl font-bold tracking-tight text-gray-900">{i.title}</h5>
+                      <p className="text-xs">Created by <i className="text-green-500">{i.creator}</i></p>
+                      <p className="mb-3 mt-3 text-xs text-gray-400"><i className="text-pink-400">{moment(i.createdAt).fromNow()}</i></p>
+                        
+                      <p className="mb-3 mt-3 font-normal text-gray-700">{i.message}</p>
+                      <p className="mb-3 mt-3 text-xs text-gray-700"><i className="text-gray-400">{i.date}</i></p>
+                      <button 
+                        className="absolute bottom-0 right-0 h-16 w-16 px-3 py-2 text-sm font-medium text-center text-red-600 rounded-lg hover:text-red-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        onClick={() => handleDelete(i)}
+                      >
+                        delete
+                      </button>
+                    </div>
+                </div>
+              )
+            })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div> 
   )
 }
